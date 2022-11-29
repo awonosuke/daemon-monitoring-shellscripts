b6aa7be1754fc1283188c91b1ef77cbc6ec65703
awonosuke <blueno091117@gmail.com>
2022-11-30 04:21:12 +0900

    [add] postgres health check script

---
+++ db-daemon-monitoring/postgresql-alive-monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+IS_ALIVE="accepting connections"
+RESULT=`pg_isready -h hostname -p portnumber 2>&1`
+
+# "accepting connections"が含まれてない(終了ステータスが0以外)場合、slackに通知
+echo "$RESULT" | grep "$IS_ALIVE" > /dev/null 2>&1
+if [ $? != 0 ]; then
+curl=`cat <<EOS
+curl
+  -X POST
+  https://hooks.slack.com/services/xxx
+  -H 'Content-Type: application/json'
+  --data '{"text": "<!channel> postgres is dead"}'
+EOS`
+eval $curl > /dev/null 2>&1
+fi
